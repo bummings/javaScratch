8b7a0cdb9e9ec4bbcd3aed62b6e39715558bb8be
bummings <eabummings@gmail.com>
2024-06-29 13:47:01 -0400

    8 complete

--- scratch.java
+++ scratch.java
@@ -1,6 +1,3 @@
-// import java.util.ArrayList;
-// import java.util.Collections;
-// import java.util.Scanner;
 import java.util.*;
 
 
@@ -8,46 +5,89 @@ public class scratch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-// Write a program that collects any number of non-negative integer inputs and 
-// calculates the sum of the values. A negative integer should end the input collection and 
-// is not part of the sum.
-// Output the smallest non-negative value and the sum of the non-negative input values, ending 
-// with a newline. Ensure your program output matches the example formatting below and works for 
-// a variety of input values.
-// If the input is:
-// 15
-// 20
-// 0
-// 3
-// -1
-// the output is:
-// Smallest: 0
-// Sum: 38
-
-    ArrayList<Integer> numbers = new ArrayList<>();
-    System.out.println("Please enter a number: ");
-    while (true) {
-        int input = scanner.nextInt();
         
+    
+            // 8. Write a program that creates an array to hold three values of type double. The program 
+            // should collect the three double values as input and store them in the array. Then calculate 
+            // the average value of the array.
+            // Output the array values and calculated average value, ending with a newline. Ensure your 
+            // program output matches the example formatting below and works for a variety of input values.
+            // If the input is:
+            // 10.0
+            // 10.5
+            // 11.0
+            // the output is:
+            // Array items: 10.0, 10.5, 11.0
+            // Average: 10.5
 
-        if (input >= 0) {
-            numbers.add(input);
-        }
-        if (input == -1) {
-            int smallest = Collections.min(numbers);
-            System.out.println("Smallest: " + smallest);
+            // ArrayList<Double> numbers = new ArrayList<>(3);
+
+            // System.out.println("Please input three integers: ");
+
+            // while (numbers.size() < 3 ) {
+            //     double input = scanner.nextDouble();
+            //     numbers.add(input);
+            // } 
+            // double sum = 0;
+            // for (double number : numbers) {
+            //     sum += number;
+            // }
             
-            int sum = 0;
-            for (int number : numbers) {
-                sum += number;
-            };
+            // double average = sum / numbers.size();
 
-            System.out.println("Sum: " + sum);
-            // System.out.println("Sum: ");
-            break;
-        }
-        System.out.println(numbers);
-    }
+            // // convert array to a string to remove brackets
+            // String output = numbers.stream()
+            //                         .map(String::valueOf)
+            //                         .collect(Collectors.joining(", "));
+
+
+            
+            // System.out.println("Array items: " + output);
+            // System.out.println("Average: " + average);
+    
+
+
+
+            // 7. Write a program that collects any number of non-negative integer inputs and 
+            // calculates the sum of the values. A negative integer should end the input collection and 
+            // is not part of the sum.
+            // Output the smallest non-negative value and the sum of the non-negative input values, ending 
+            // with a newline. Ensure your program output matches the example formatting below and works for 
+            // a variety of input values.
+            // If the input is:
+            // 15
+            // 20
+            // 0
+            // 3
+            // -1
+            // the output is:
+            // Smallest: 0
+            // Sum: 38
+
+            // ArrayList<Integer> numbers = new ArrayList<>();
+            // System.out.println("Please enter a number: ");
+            // while (true) {
+            //     int input = scanner.nextInt();
+                
+
+            //     if (input >= 0) {
+            //         numbers.add(input);
+            //     }
+            //     if (input == -1) {
+            //         int smallest = Collections.min(numbers);
+            //         System.out.println("Smallest: " + smallest);
+                    
+            //         int sum = 0;
+            //         for (int number : numbers) {
+            //             sum += number;
+            //         };
+
+            //         System.out.println("Sum: " + sum);
+            //         // System.out.println("Sum: ");
+            //         break;
+            //     }
+            //     System.out.println(numbers);
+            // }
 
 
             // 6. Write a program that collects a full name as one string input.
@@ -115,32 +155,32 @@ public class scratch {
         
 
 
-//         /* 4. Write a program that takes a full name, age, and salary 
-//         as inputs on separate lines. Output a formatted message 
-//         containing the inputs, ending with a newline. Ensure your program 
-//         output matches the example formatting below and works for a variety 
-//         of input values.
-//         If the input is:
-//             Pat Ford
-//             35
-//             60,000
-//         the output is:
-//             Pat Ford is 35 and makes $60,000.
-// */
-//         String name = "";
-//         int age = 0;
-//         int salary = 0;
-        
-        
-//         System.out.print("name: \n");
-//         name = scanner.nextLine();
-//         System.out.print("age: \n");
-//         age = scanner.nextInt();
-//         System.out.print("salary: \n");
-//         salary = scanner.nextInt();
-        
+            //  4. Write a program that takes a full name, age, and salary 
+            //  as inputs on separate lines. Output a formatted message 
+            //  containing the inputs, ending with a newline. Ensure your program 
+            //  output matches the example formatting below and works for a variety 
+            //  of input values.
+            //  If the input is:
+            //      Pat Ford
+            //      35
+            //      60,000
+            //  the output is:
+            //      Pat Ford is 35 and makes $60,000.
+            //
+            //  String name = "";
+            //  int age = 0;
+            //  int salary = 0;
+            
+            
+            //  System.out.print("name: \n");
+            //  name = scanner.nextLine();
+            //  System.out.print("age: \n");
+            //  age = scanner.nextInt();
+            //  System.out.print("salary: \n");
+            //  salary = scanner.nextInt();
+            
 
-//         System.out.println(name + "is " + age + " and makes " + salary +".");
+            //  System.out.println(name + "is " + age + " and makes " + salary +".");
     
 
         scanner.close();
