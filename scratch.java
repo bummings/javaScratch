eac44a458149a1912413d931e95ed4009e41c96d
bummings <eabummings@gmail.com>
2024-07-05 16:30:50 -0400

    wow that was crazy how you had to add a .nextLine() method after the nextInt() method

--- scratch.java
+++ scratch.java
@@ -1,14 +1,35 @@
+// Write a program that takes a full name, age, and salary as inputs on separate lines. Output a formatted message containing the inputs, ending with a newline. Ensure your program output matches the example formatting below and works for a variety of input values.
+
+// If the input is:
+
+// Pat Ford
+// 35
+// 60,000
+// the output is:
+
+// Pat Ford is 35 and makes $60,000.
+
 import java.util.*;
+
 public class scratch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
               
         String name = "";
         int age = 0;
-        int salary = 0;
+        String salary = "";
         
         name = scanner.nextLine();
         System.out.println(name);
+        
+        age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(age);
+        
+        salary = scanner.nextLine();
+        System.out.println(salary + "\n");
+        
+        System.out.println(name + " is " + age + " and makes " + salary + ".");
               
         scanner.close();
     }
